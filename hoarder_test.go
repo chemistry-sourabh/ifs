118426bdf227c472333f9134d0fc0feec140f1af
Sourabh Bollapragada <chemistry_sourabh@yahoo.co.in>
2018-08-05 19:50:29 -0400

    fixed test

--- hoarder_test.go
+++ hoarder_test.go
@@ -14,7 +14,7 @@ func TestHoarder_GetCacheFileName(t *testing.T) {
 
 	for i := 0; i < 5; i++ {
 		p := h.GetCacheFileName()
-		Compare(t, p, strconv.FormatInt(int64(i+1), 10))
+		Compare(t, p, strconv.FormatInt(int64(i+2), 10))
 	}
 
 }
